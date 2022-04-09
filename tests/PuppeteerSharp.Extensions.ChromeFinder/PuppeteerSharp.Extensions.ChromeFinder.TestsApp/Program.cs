ed2a5b498a55a0373c275e293a84c213d8aec7a5
gfys-rus <gfys-rus@mail.ru>
2022-04-09 13:27:14 +0300

    Chrome finder tests

---
+++ tests/PuppeteerSharp.Extensions.ChromeFinder/PuppeteerSharp.Extensions.ChromeFinder.TestsApp/Program.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace PuppeteerSharp.Extensions.ChromeFinder.TestsApp
+{
+    internal class Program
+    {
+        private static async Task Main(string[] args)
+        {
+            var browserFetcher = new BrowserFetcher(new BrowserFetcherOptions
+            {
+                Product = Product.Chrome
+            });
+            await browserFetcher.DownloadAsync();
+
+            var chromeFinder = new ChromeFinder();
+            var chromePath = chromeFinder.Find();
+
+            var chromeDataDirPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "chromeDataDir");
+
+            var launchOptions = new LaunchOptions
+            {
+                Headless = false,
+                UserDataDir = chromeDataDirPath,
+                //ExecutablePath = "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe",
+                ExecutablePath = chromePath[0],
+                IgnoreDefaultArgs = true,
+                IgnoreHTTPSErrors = true,
+                DefaultViewport = null,
+                Args = new[]
+                {
+                    #region UI/UX part.
+                  
+                    "--app=https://initializing.current.application",
+                    "--disable-notifications",
+                    "--window-size=960,540",
+                    "--compensate-for-unstable-pinch-zoom",
+                    "--content-shell-hide-toolbar",
+                    "--disable-dinosaur-easter-egg",
+                    "--disable-device-discovery-notifications",
+                    "--force-dark-mode",
+                    "--mute-audio",
+                    "--no-initial-navigation",
+                    "--use-mobile-user-agent",
+                    //"--force-tablet-mode",
+                    //"--force-enable-night-mode",
+                    //"--force-android-app-mode",
+                    //"--force-app-mode",
+                    //"--top-controls-hide-threshold=0",
+                    //"--cast-app-background-color=ffffffff",
+                    //"--default-background-color=ffffffff",
+                    #endregion
+
+                    #region Performance.
+                    "--enable-accelerated-2d-canvas",
+                    "--force-gpu-rasterization",
+                    "--disable-auto-reload",
+                    "--disable-crash-reporter",
+                    "--disable-logging",
+                    "--enable-defer-all-script-without-optimization-hints",
+                    "--no-crash-upload",
+                    #endregion
+
+                    #region Security.
+                    "--webview-disable-safebrowsing-support",
+                    "--unlimited-storage",
+                    "--allow-cross-origin-auth-prompt",
+                    "--allow-external-pages",
+                    "--allow-failed-policy-fetch-for-test",
+                    "--allow-file-access-from-files",
+                    "--allow-file-access",
+                    "--allow-http-background-page",
+                    "--allow-http-screen-capture",
+                    "--allow-insecure-localhost",
+                    "--allow-legacy-extension-manifests",
+                    "--allow-loopback-in-peer-connection",
+                    "--allow-no-sandbox-job",
+                    "--allow-outdated-plugins",
+                    "--allow-running-insecure-content",
+                    "--allow-sandbox-debugging",
+                    "--allow-silent-push",
+                    //"--allow-third-party-modules",
+                    "--allow-unsecure-dlls",
+                    "--app-shell-allow-roaming",
+                    "--disable-client-side-phishing-detection",
+                    "--disable-site-isolation-for-policy",
+                    //"--disable-web-security",
+                    "--disable-windows10-custom-titlebar",
+                    "--enable-experimental-extension-apis",
+                    "--enable-experimental-ui-automation",
+                    "--enable-experimental-webassembly-features",
+                    "--enable-ftp",
+                    "--enable-hung-renderer-infobar",
+                    "--enable-local-file-accesses",
+                    "--incognito",
+                    "",
+                    "",
+                    "",
+                    "",
+                    "",
+                    "",
+                    "",
+                    "",
+                    ""
+                    #endregion
+                },
+            };
+            var browser = await Puppeteer.LaunchAsync(launchOptions);
+            AppDomain.CurrentDomain.ProcessExit += delegate
+            {
+                browser.CloseAsync();
+            };
+            browser.Closed += delegate
+            {
+                var myProcess = Process.GetCurrentProcess();
+                myProcess.CloseMainWindow();
+                myProcess.Close();
+                System.Environment.Exit(0);
+            };
+
+            var pages = await browser.PagesAsync();
+            var page = pages[0]; ;
+            await page.GoToAsync("http://www.google.com");
+            //await page.ScreenshotAsync("C:\\Users\\Yura\\Desktop\\puppy.jpg");
+
+            while (true)
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ tests/PuppeteerSharp.Extensions.ChromeFinder/PuppeteerSharp.Extensions.ChromeFinder/Class1.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace PuppeteerSharp.Extensions.ChromeFinder
+{
+    public class ChromeFinder
+    {
+        public IList<string> Find()
+        {
+
+            var suffixes = new string[]
+            {
+                "\\Google\\Chrome SxS\\Application\\chrome.exe",
+                "\\Google\\Chrome\\Application\\chrome.exe",
+                "\\chrome-win32\\chrome.exe",
+                "\\Chromium\\Application\\chrome.exe",
+                "\\Google\\Chrome Beta\\Application\\chrome.exe"
+            };
+
+            var prefixes = new string[]
+            {
+                "C:\\Program Files",
+                "D:\\Program Files",
+                "C:\\Program Files (x86)",
+                "D:\\Program Files (x86)"
+            };
+
+            var accessibleFiles = new List<string>();
+            foreach (var pref in prefixes)
+            {
+                foreach (var suf in suffixes)
+                {
+                    var filePath = pref + suf;
+                    if (CanAccess(filePath))
+                    {
+                        accessibleFiles.Add(filePath);
+                    }
+                }
+            }
+            return accessibleFiles;
+        }
+
+        private bool CanAccess(string filePath)
+        {
+            if (!File.Exists(filePath))
+            {
+                return false;
+            }
+            if (IsFileLocked(filePath))
+            {
+                return false;
+            }
+            return true;
+        }
+
+        private bool IsFileLocked(string filePath)
+        {
+            try
+            {
+                using (FileStream stream = File.Open(filePath, FileMode.Open, FileAccess.Read, FileShare.None))
+                {
+                    stream.Close();
+                }
+            }
+            catch (IOException)
+            {
+                //the file is unavailable because it is:
+                //still being written to
+                //or being processed by another thread
+                //or does not exist (has already been processed)
+                return true;
+            }
+
+            //file is not locked
+            return false;
+        }
+
+    }
+}
